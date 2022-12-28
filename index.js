cd6a1f0e9e2558de6f1b22ae020ddb649e839e69
Simen A. W. Olsen <so@bjerk.io>
2022-12-28 21:15:17 +0100

    feat: allow template literals
    Reasoning behind my change here is mainly jest snapshots. In most projects I work on, there are inline snapshots (which typically looks like this: `"hello world"`). I feel we should allow for this.
    
    Would love to hear feedback!

--- index.js
+++ index.js
@@ -93,6 +93,7 @@ module.exports = {
         '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/no-unused-vars': 'off',
         '@typescript-eslint/no-var-requires': 'off',
+        quotes: ['error', 'single', { allowTemplateLiterals: true }],
       },
     },
   ],
